cbcf004bbb3295940abed6c4c6f5a5196ce3c81c
Bernardo Amorim <contato@bamorim.com>
2018-07-19 18:23:59 -0300

    feat: keep nil when type is optional

--- lib/coercions/optional.ex
+++ lib/coercions/optional.ex
@@ -5,11 +5,14 @@ defmodule ExchemaCoercion.Coercions.Optional do
 
   @doc """
   Coerces to an optional type.
-  It just coerces the inner type as normal.
+  It just coerces the inner type as normal, unless the inside is nil.
   """
+  def optional(nil, {Exchema.Types.Optional, _}, _) do
+    {:ok, nil}
+  end
   def optional(input, {Exchema.Types.Optional, inner}, coercions) do
     ExchemaCoercion.try_coerce(input, inner, coercions)
   end
-  
+
   def optional(_, _, _), do: :error
 end
